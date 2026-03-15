c7c23f07de433fc57d50f168ee17f8c838c3771d
Iago <iago.silva6969@gmail.com>
2026-03-15 19:02:40 -0300

    controles employee criados

--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -5,7 +5,7 @@ import { AppService } from '../services/app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('/hello')
+  @Get('/')
   getHello(): string {
     return this.appService.getHello();
   }
