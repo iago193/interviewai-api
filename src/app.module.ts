e3843a073067564334d175bf66b84deb833160c8
Iago <iago.silva6969@gmail.com>
2026-03-18 18:54:11 -0300

    refactore nos modulos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaModule } from './prisma/prisma.module';
-import { EmployeeController } from './employee/employee.controller';
 import { EmployeeModule } from './employee/employee.module';
 
 @Module({
-  imports: [PrismaModule, EmployeeModule],
-  controllers: [AppController, EmployeeController],
+  imports: [EmployeeModule],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
